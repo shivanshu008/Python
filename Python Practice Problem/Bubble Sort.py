1db88f715ab3780b26020eeccd04266b5ee239df
shivanshu008 <gauravshivanshu8@gmail.com>
2024-10-15 13:37:16 +0530

    update

---
+++ Python Practice Problem/Bubble Sort.py
@@ -0,0 +1,16 @@
+## Implimentation of Bubble sort
+## Method Implimentation
+## Time Complexity : O (n ^ 2)
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range (n):
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                ## swap of the element
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
+
+## Driver Code
+arr = [70,20,50,60,35,47]
+result = bubbleSort(arr)
+print("Array after using bubble short is: ",result)
---
+++ Python Practice Problem/Selection Sort.py
@@ -0,0 +1,15 @@
+## Method Definition
+def SelectionSort(arr):
+    n = len(arr)
+    for i in range (n):
+        min_index = i
+        for j in range (i+1,n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+                arr[i],arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+## Driver Code
+arr = [70,56,23,19,25,37,48]
+result = SelectionSort(arr)
+print("Sorted array after applying Selection Sort: ",result)
